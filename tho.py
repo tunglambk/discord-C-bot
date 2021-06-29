530131fa22cfd987c32890688b5c75d44c1f1f22
Tung Lam <lamphambatung92@gmail.com>
2021-06-29 18:54:38 +0700

    fix tho.py file

--- tho.py
+++ tho.py
@@ -1,9 +1,8 @@
 tho = [
-    '> Sao anh không về chơi thôn Vĩ?\n> > Nhìn nắng hàng cau nắng mới lên.\n> > Vườn ai mướt quá xanh như ngọc\n> > Lá trúc che ngang mặt chữ điền.',
-    '> Mỗi năm hoa đào nở\n> > Lại thấy ông đồ già\n> > Bày mực tàu, giấy đỏ\n> > Bên phố đông người qua',
-    '> Trăm năm trong cõi người ta,\n> > Chữ tài chữ mệnh khéo là ghét nhau.\n> > Trải qua một cuộc bể dâu,\n> > Những điều trông thấy mà đau đớn lòng.',
-    '> Một chàng vừa trạc thanh xuân,
-    '> Hình dong chải chuốt, áo khăn dịu dàng.\n> > Nghĩ rằng cũng mạch thư hương,\n> > Hỏi ra mới biết rằng chàng Sở Khanh.\n> > Bóng Nga thấp thoáng dưới mành,\n> > Trông nàng, chàng cũng ra tình đeo đai.'
+    '> Sao anh không về chơi thôn Vĩ?\n> Nhìn nắng hàng cau nắng mới lên.\n> Vườn ai mướt quá xanh như ngọc\n> Lá trúc che ngang mặt chữ điền.',
+    '> Mỗi năm hoa đào nở\n> Lại thấy ông đồ già\n> Bày mực tàu, giấy đỏ\n> Bên phố đông người qua',
+    '> Trăm năm trong cõi người ta,\n> Chữ tài chữ mệnh khéo là ghét nhau.\n> Trải qua một cuộc bể dâu,\n> Những điều trông thấy mà đau đớn lòng.',
+    '> Một chàng vừa trạc thanh xuân,\n> Hình dong chải chuốt, áo khăn dịu dàng.\n> Nghĩ rằng cũng mạch thư hương,\n> Hỏi ra mới biết rằng chàng Sở Khanh.\n> Bóng Nga thấp thoáng dưới mành,\n> Trông nàng, chàng cũng ra tình đeo đai.'
     '> Sóng gợn tràng giang buồn điệp điệp,\n> Con thuyền xuôi mái nước song song.\n> Thuyền về nước lại, sầu trăm ngả;\n> Củi một cành khô lạc mấy dòng'
     '> Gậm một khối căm hờn trong cũi sắt,\n> Ta nằm dài, trông ngày tháng dần qua.\n> Khinh lũ người kia ngạo mạn, ngẩn ngơ,\n> Giương mắt bé diễu oai linh rừng thẳm'
     '> Có biết chăng trong những ngày ngao ngán,\n> Ta đương theo giấc mộng ngàn to lớn\n> Để hồn ta phảng phất được gần ngươi,\n> Hỡi cảnh rừng ghê gớm của ta ơi!'
