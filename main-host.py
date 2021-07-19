aa86036d4e7d80e573eeb703bec44a9369f50de1
Tung Lam <lamphambatung92@gmail.com>
2021-07-19 23:15:37 +0700

    test bug

--- main-host.py
+++ main-host.py
@@ -1,116 +1,116 @@
 import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
-import nltk 
-nltk.download('punkt')
+# import nltk 
+# nltk.download('punkt')
 
-from nltk import word_tokenize,sent_tokenize
+# from nltk import word_tokenize,sent_tokenize
 
-from nltk.stem.lancaster import LancasterStemmer
-stemmer = LancasterStemmer()
+# from nltk.stem.lancaster import LancasterStemmer
+# stemmer = LancasterStemmer()
 
-import numpy as np 
-import tflearn
-import tensorflow as tf
-import random
-import json
-import pickle
-import os
-from tho import tho
-from nofap import nofap
-from truyencuoi import truyencuoi
-from facepplib import FacePP
-import time
-import requests
+# import numpy as np 
+# import tflearn
+# import tensorflow as tf
+# import random
+# import json
+# import pickle
+# import os
+# from tho import tho
+# from nofap import nofap
+# from truyencuoi import truyencuoi
+# from facepplib import FacePP
+# import time
+# import requests
 
-if os.path.isfile("data.pickle"):
-    os.remove("data.pickle")
+# if os.path.isfile("data.pickle"):
+#     os.remove("data.pickle")
 
-with open("intents.json") as file:
-    data = json.load(file)
+# with open("intents.json") as file:
+#     data = json.load(file)
 
-try:
-    with open("data.pickle","rb") as f:
-        words, labels, training, output = pickle.load(f)
+# try:
+#     with open("data.pickle","rb") as f:
+#         words, labels, training, output = pickle.load(f)
 
-except:
-    words = []
-    labels = []
-    docs_x = []
-    docs_y = []
-    for intent in data["intents"]:
-        for pattern in intent["patterns"]:
-            wrds = nltk.word_tokenize(pattern)
-            words.extend(wrds)
-            docs_x.append(wrds)
-            docs_y.append(intent["tag"])
+# except:
+#     words = []
+#     labels = []
+#     docs_x = []
+#     docs_y = []
+#     for intent in data["intents"]:
+#         for pattern in intent["patterns"]:
+#             wrds = nltk.word_tokenize(pattern)
+#             words.extend(wrds)
+#             docs_x.append(wrds)
+#             docs_y.append(intent["tag"])
             
-        if intent["tag"] not in labels:
-            labels.append(intent["tag"])
+#         if intent["tag"] not in labels:
+#             labels.append(intent["tag"])
 
 
-    words = [stemmer.stem(w.lower()) for w in words if w != "?"]
-    words = sorted(list(set(words)))
-    labels = sorted(labels)
+#     words = [stemmer.stem(w.lower()) for w in words if w != "?"]
+#     words = sorted(list(set(words)))
+#     labels = sorted(labels)
 
-    training = []
-    output = []
-    out_empty = [0 for _ in range(len(labels))]
+#     training = []
+#     output = []
+#     out_empty = [0 for _ in range(len(labels))]
 
-    for x, doc in enumerate(docs_x):
-        bag = []
+#     for x, doc in enumerate(docs_x):
+#         bag = []
 
-        wrds = [stemmer.stem(w.lower()) for w in doc]
+#         wrds = [stemmer.stem(w.lower()) for w in doc]
 
-        for w in words:
-            if w in wrds:
-               bag.append(1)
-            else:
-              bag.append(0)
+#         for w in words:
+#             if w in wrds:
+#                bag.append(1)
+#             else:
+#               bag.append(0)
     
-        output_row = out_empty[:]
-        output_row[labels.index(docs_y[x])] = 1
+#         output_row = out_empty[:]
+#         output_row[labels.index(docs_y[x])] = 1
         
-        training.append(bag)
-        output.append(output_row)
+#         training.append(bag)
+#         output.append(output_row)
 
-    training = np.array(training)
-    output = np.array(output)
+#     training = np.array(training)
+#     output = np.array(output)
     
-    with open("data.pickle","wb") as f:
-        pickle.dump((words, labels, training, output), f)
+#     with open("data.pickle","wb") as f:
+#         pickle.dump((words, labels, training, output), f)
 
 
 
-net = tflearn.input_data(shape=[None, len(training[0])])
-net = tflearn.fully_connected(net, 8)
-net = tflearn.fully_connected(net, 8)
-net = tflearn.fully_connected(net, len(output[0]), activation="softmax")
-net = tflearn.regression(net)
+# net = tflearn.input_data(shape=[None, len(training[0])])
+# net = tflearn.fully_connected(net, 8)
+# net = tflearn.fully_connected(net, 8)
+# net = tflearn.fully_connected(net, len(output[0]), activation="softmax")
+# net = tflearn.regression(net)
 
-model = tflearn.DNN(net)
-model.fit(training, output, n_epoch=1000, batch_size=8, show_metric=True)
-model.save("model.tflearn")
+# model = tflearn.DNN(net)
+# model.fit(training, output, n_epoch=1000, batch_size=8, show_metric=True)
+# model.save("model.tflearn")
 
-try:
-    model.load("model.tflearn")
-except:
-    model.fit(training, output, n_epoch=1000, batch_size=8, show_metric=True)
-    model.save("model.tflearn")
+# try:
+#     model.load("model.tflearn")
+# except:
+#     model.fit(training, output, n_epoch=1000, batch_size=8, show_metric=True)
+#     model.save("model.tflearn")
 
 
-def bag_of_words(s, words):
-    bag = [0 for _ in range(len(words))]
+# def bag_of_words(s, words):
+#     bag = [0 for _ in range(len(words))]
 
-    s_words = nltk.word_tokenize(s)
-    s_words = [stemmer.stem(word.lower()) for word in s_words]
+#     s_words = nltk.word_tokenize(s)
+#     s_words = [stemmer.stem(word.lower()) for word in s_words]
 
-    for se in s_words:
-        for i, w in enumerate(words):
-            if w == se:
-                bag[i] = 1
+#     for se in s_words:
+#         for i, w in enumerate(words):
+#             if w == se:
+#                 bag[i] = 1
     
-    return np.array(bag)
+#     return np.array(bag)
 
 
 import discord
@@ -167,7 +167,8 @@ class MyClient(discord.Client):
     async def on_message(self, message):
 
         #if message.channel.name=="test-con-bot-cua-tung":
-        if str(message.guild.id) == "769800684056215572" and str(message.author.id) == "546463922287411230":
+        
+        if str(message.guild.id) == "769800684056215572":# str(message.author.id) == "546463922287411230":
             if "The Giveaway of" in message.content and "has ended!" in message.content:
                 amount = message.content.split('to the Winners of', 1)[1]
                 amount = amount.split('ark each', 1)[0]
@@ -516,18 +517,18 @@ class MyClient(discord.Client):
                 return
 
 
-            inp = message.content
-            result = model.predict([bag_of_words(inp, words)])[0]
-            result_index = np.argmax(result)
-            tag = labels[result_index]
-            print(result[result_index])
-            if result[result_index] > 0.925:
-                for tg in data["intents"]:
-                    if tg['tag'] == tag:
-                        responses = tg['responses']
+            # inp = message.content
+            # result = model.predict([bag_of_words(inp, words)])[0]
+            # result_index = np.argmax(result)
+            # tag = labels[result_index]
+            # print(result[result_index])
+            # if result[result_index] > 0.925:
+            #     for tg in data["intents"]:
+            #         if tg['tag'] == tag:
+            #             responses = tg['responses']
             
-                bot_response=random.choice(responses)
-                await message.channel.send('> ' + bot_response.format(message))
+            #     bot_response=random.choice(responses)
+            #     await message.channel.send('> ' + bot_response.format(message))
 
 
 api_key_face = os.getenv("FACE_KEY")
